e50df60f0b95af9d4d00cba7b686c106d18a127b
w-kamil <w-kamil@wp.pl>
2017-06-07 16:08:04 +0200

    work with layouts

---
+++ app/src/main/java/com/github/w_kamil/walizka/MainListAdapter.java
@@ -0,0 +1,10 @@
+package com.github.w_kamil.walizka;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Frod_ on 07.06.2017.
+ */
+
+public class MainListAdapter extends RecyclerView.Adapter {
+}
--- app/src/main/java/com/github/w_kamil/walizka/packingList/ListActivity.java
+++ app/src/main/java/com/github/w_kamil/walizka/packingList/ListActivity.java
@@ -1,7 +1,9 @@
-package com.github.w_kamil.walizka;
+package com.github.w_kamil.walizka.packingList;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.w_kamil.walizka.R;
 
 public class ListActivity extends AppCompatActivity {
 
